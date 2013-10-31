f4c8d45cc48b4e142cba1aa7d4075a07e2cd85bd
IcedReaper <Kevin.Lang@gmx.de>
2013-10-31 12:53:56 +0100

    Added module prefix into tablenames for the tables of the module irBlog

--- sql/postgres/modules/irBlog/mainTables.sql
+++ sql/postgres/modules/irBlog/mainTables.sql
@@ -1,40 +1,41 @@
-﻿CREATE TABLE ircms_irblog_blog (
-  blogid        serial,
-  userid        integer NOT NULL,
+﻿CREATE TABLE irCMS_modules_irBlog_blog (
+  blogId        serial,
+  userId        integer NOT NULL,
   active        boolean NOT NULL DEFAULT false,
-  releasedate   timestamp with time zone DEFAULT now(),
+  releaseDate   timestamp with time zone DEFAULT now(),
   title         text    NOT NULL,
   subtitle      text,
   content       text,
   views         integer NOT NULL DEFAULT 0,
   rating        integer NOT NULL DEFAULT 0,
-  ratingcounter integer NOT NULL DEFAULT 0,
+  ratingCounter integer NOT NULL DEFAULT 0,
 
-  CONSTRAINT "PK_irCMS_irBlog_blog_blogId" PRIMARY KEY (blogid),
-  CONSTRAINT "FK_irCMS_irBlog_blog_userId" FOREIGN KEY (userid) REFERENCES ircms_user (userid) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT "UK_irCMS_irBlog_blog_title"  UNIQUE      (title)
+  CONSTRAINT "PK_irCMS_modules_irBlog_blog_blogId" PRIMARY KEY (blogid),
+  CONSTRAINT "FK_irCMS_modules_irBlog_blog_userId" FOREIGN KEY (userid) REFERENCES irCMS_user (userid) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "UK_irCMS_modules_irBlog_blog_title"  UNIQUE      (title)
 );
-CREATE INDEX "FKI_irCMS_irBlog_blog_userId"        ON ircms_irblog_blog USING btree (userid);
-CREATE INDEX "IDX_irCMS_irBlog_blog_rating"        ON ircms_irblog_blog USING btree (rating);
-CREATE INDEX "IDX_irCMS_irBlog_blog_ratingCounter" ON ircms_irblog_blog USING btree (ratingcounter);
-CREATE INDEX "IDX_irCMS_irBlog_blog_views"         ON ircms_irblog_blog USING btree (views);
+CREATE INDEX "FKI_irCMS_modules_irBlog_blog_userId"        ON irCMS_modules_irBlog_blog USING btree (userid);
+CREATE INDEX "IDX_irCMS_modules_irBlog_blog_rating"        ON irCMS_modules_irBlog_blog USING btree (rating);
+CREATE INDEX "IDX_irCMS_modules_irBlog_blog_ratingCounter" ON irCMS_modules_irBlog_blog USING btree (ratingcounter);
+CREATE INDEX "IDX_irCMS_modules_irBlog_blog_views"         ON irCMS_modules_irBlog_blog USING btree (views);
 
-CREATE TABLE ircms_irblog_tag (
+CREATE TABLE ircms_modules_irBlog_tag (
   tagid   serial,
-  tagname text   NOT NULL,
+  tagName text   NOT NULL,
 
-  CONSTRAINT "PK_irCMS_irBlog_tag_tagId"   PRIMARY KEY (tagid),
-  CONSTRAINT "UK_irCMS_irBlog_tag_tagName" UNIQUE      (tagname)
+  CONSTRAINT "PK_irCMS_modules_irBlog_tag_tagId"   PRIMARY KEY (tagId),
+  CONSTRAINT "UK_irCMS_modules_irBlog_tag_tagName" UNIQUE      (tagName)
 );
+CREATE INDEX "IDX_irCMS_modules_irBlog_tag_tagName" ON ircms_modules_irBlog_tag USING btree (tagName);
 
-CREATE TABLE ircms_irblog_blogtag (
-  blogtagid serial,
-  blogid    integer NOT NULL,
-  tagid     integer NOT NULL,
+CREATE TABLE irCMS_modules_irBlog_blogTag (
+  blogTagId serial,
+  blogId    integer NOT NULL,
+  tagId     integer NOT NULL,
 
-  CONSTRAINT "PK_ircms_irBlog_blogTag_blogTagId" PRIMARY KEY (blogtagid),
-  CONSTRAINT "FK_ircms_irBlog_blogTag_blogId"    FOREIGN KEY (blogid)    REFERENCES ircms_irblog_blog (blogid) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT "FK_ircms_irBlog_blogTag_tagId"     FOREIGN KEY (tagid)     REFERENCES ircms_irblog_tag  (tagid)  MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+  CONSTRAINT "PK_irCMS_modules_irBlog_blogTag_blogTagId" PRIMARY KEY (blogtagid),
+  CONSTRAINT "FK_irCMS_modules_irBlog_blogTag_blogId"    FOREIGN KEY (blogid)    REFERENCES ircms_modules_irBlog_blog (blogid) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "FK_irCMS_modules_irBlog_blogTag_tagId"     FOREIGN KEY (tagid)     REFERENCES ircms_modules_irBlog_tag  (tagid)  MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
-CREATE INDEX "FKI_ircms_irBlog_blogTag_blogId" ON ircms_irblog_blogtag USING btree (blogid);
-CREATE INDEX "FKI_ircms_irBlog_blogTag_tagId"  ON ircms_irblog_blogtag USING btree (tagid);
+CREATE INDEX "FKI_irCMS_modules_irBlog_blogTag_blogId" ON irCMS_modules_irblog_blogTag USING btree (blogid);
+CREATE INDEX "FKI_irCMS_modules_irBlog_blogTag_tagId"  ON irCMS_modules_irblog_blogTag USING btree (tagid);
