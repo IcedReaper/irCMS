183edfa834a74c5b9d31bab13cb646dda97156ea
IcedReaper <Kevin.Lang@gmx.de>
2013-10-31 13:46:14 +0100

    Added module prefix into tablenames for the tables of the module irModule Added Indecies

--- sql/postgres/modules/irModules/mainTables.sql
+++ sql/postgres/modules/irModules/mainTables.sql
@@ -1,13 +1,15 @@
-﻿CREATE TABLE irCMS_irModules_status (
+﻿CREATE TABLE irCMS_modules_irModule_status (
 	statusId   serial,
 	statusName text    NOT NULL,
 	online     boolean NOT NULL DEFAULT false,
 
-	CONSTRAINT "PK_irCMS_irModules_status_statusId" PRIMARY KEY (statusId),
-	CONSTRAINT "UK_irCMS_irModules_status_statusName" UNIQUE (statusName)
+	CONSTRAINT "PK_irCMS_modules_irModule_status_statusId"   PRIMARY KEY (statusId),
+	CONSTRAINT "UK_irCMS_modules_irModule_status_statusName" UNIQUE      (statusName)
 );
+CREATE INDEX "IDX_irCMS_modules_irModule_status_statusName"       ON irCMS_modules_irModule_status USING btree (statusName);
+CREATE INDEX "IDX_irCMS_modules_irModule_status_statusNameOnline" ON irCMS_modules_irModule_status USING btree (statusName, online);
 
-CREATE TABLE irCMS_irModules_entity (
+CREATE TABLE irCMS_modules_irModule_entity (
 	entityId     serial,
 	entityName   text    NOT NULL,
 	active       boolean NOT NULL DEFAULT false,
@@ -19,17 +21,17 @@ CREATE TABLE irCMS_irModules_entity (
 	userName     text,
 	creationDate timestamp with time zone DEFAULT now(),
 	
-	CONSTRAINT "PK_irCMS_irModules_entity_entityId"   PRIMARY KEY (entityId),
-	CONSTRAINT "UK_irCMS_irModules_entity_entityName" UNIQUE      (entityName),
-	CONSTRAINT "FK_irCMS_irModules_entity_statusId"   FOREIGN KEY (statusId) REFERENCES irCMS_irModules_status (statusId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
-	CONSTRAINT "FK_irCMS_irModules_entity_userId"     FOREIGN KEY (userId)   REFERENCES irCMS_user             (userId)   MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+	CONSTRAINT "PK_irCMS_modules_irModule_entity_entityId"   PRIMARY KEY (entityId),
+	CONSTRAINT "UK_irCMS_modules_irModule_entity_entityName" UNIQUE      (entityName),
+	CONSTRAINT "FK_irCMS_modules_irModule_entity_statusId"   FOREIGN KEY (statusId) REFERENCES irCMS_modules_irModules_status (statusId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT "FK_irCMS_modules_irModule_entity_userId"     FOREIGN KEY (userId)   REFERENCES irCMS_user                     (userId)   MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
-CREATE INDEX "FK_irCMS_irModules_entity_active"           ON irCMS_irModules_entity USING btree (active);
-CREATE INDEX "FK_irCMS_irModules_entity_entityNameActive" ON irCMS_irModules_entity USING btree (entityName, active);
-CREATE INDEX "FKI_irCMS_irModules_entity_statusId"        ON irCMS_irModules_entity USING btree (statusId);
-CREATE INDEX "FKI_irCMS_irModules_entity_userId"          ON irCMS_irModules_entity USING btree (userId);
+CREATE INDEX "IDX_irCMS_modules_irModule_entity_active"           ON irCMS_modules_irModules_entity USING btree (active);
+CREATE INDEX "IDX_irCMS_modules_irModule_entity_entityNameActive" ON irCMS_modules_irModules_entity USING btree (entityName, active);
+CREATE INDEX "FKI_irCMS_modules_irModule_entity_statusId"         ON irCMS_modules_irModules_entity USING btree (statusId);
+CREATE INDEX "FKI_irCMS_modules_irModule_entity_userId"           ON irCMS_modules_irModules_entity USING btree (userId);
 
-CREATE TABLE irCMS_irModules_tag (
+CREATE TABLE irCMS_modules_irModule_tag (
 	tagId         serial,
 	tagName       text    NOT NULL,
 	entityCounter integer NOT NULL DEFAULT 0,
@@ -37,14 +39,14 @@ CREATE TABLE irCMS_irModules_tag (
 	userId        integer,
 	creationDate  timestamp with time zone DEFAULT now(),
 
-	CONSTRAINT "PK_irCMS_irModules_tag_tagId"   PRIMARY KEY (tagId),
-	CONSTRAINT "UK_irCMS_irModules_tag_tagName" UNIQUE      (tagName),
-	CONSTRAINT "FK_irCMS_irModules_tag_userId"  FOREIGN KEY (userId)  REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+	CONSTRAINT "PK_irCMS_modules_irModule_tag_tagId"   PRIMARY KEY (tagId),
+	CONSTRAINT "UK_irCMS_modules_irModule_tag_tagName" UNIQUE      (tagName),
+	CONSTRAINT "FK_irCMS_modules_irModule_tag_userId"  FOREIGN KEY (userId)  REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
-CREATE INDEX "FK_irCMS_irModules_tag_tagName" ON irCMS_irModules_tag USING btree (tagName);
-CREATE INDEX "FKI_irCMS_irModules_tag_userId" ON irCMS_irModules_tag USING btree (userId);
+CREATE INDEX "IDX_irCMS_modules_irModule_tag_tagName" ON irCMS_modules_irModules_tag USING btree (tagName);
+CREATE INDEX "FKI_irCMS_modules_irModule_tag_userId"  ON irCMS_modules_irModules_tag USING btree (userId);
 
-CREATE TABLE irCMS_irModules_entityTag (
+CREATE TABLE irCMS_modules_irModule_entityTag (
 	entityTagId  serial,
 	entityId     integer NOT NULL,
 	tagId        integer NOT NULL,
@@ -52,9 +54,9 @@ CREATE TABLE irCMS_irModules_entityTag (
 	userId       integer NOT NULL,
 	creationDate timestamp with time zone DEFAULT now(),
 
-	CONSTRAINT "PK_irCMS_irModules_entityTag_entityTagId"   PRIMARY KEY (entityTagId),
-	CONSTRAINT "UK_irCMS_irModules_entityTag_entityIdTagId" UNIQUE      (entityId, tagId),
-	CONSTRAINT "FK_irCMS_irModules_entityTag_userId"        FOREIGN KEY (userId)          REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+	CONSTRAINT "PK_irCMS_modules_irModule_entityTag_entityTagId"   PRIMARY KEY (entityTagId),
+	CONSTRAINT "UK_irCMS_modules_irModule_entityTag_entityIdTagId" UNIQUE      (entityId, tagId),
+	CONSTRAINT "FK_irCMS_modules_irModule_entityTag_userId"        FOREIGN KEY (userId)          REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
-CREATE INDEX "FK_irCMS_irModules_entityTag_entityIdTagId" ON irCMS_irModules_entityTag USING btree (entityId, tagId);
-CREATE INDEX "FKI_irCMS_irModules_entityTag_userId"       ON irCMS_irModules_entityTag USING btree (userId);
+CREATE INDEX "IDX_irCMS_modules_irModule_entityTag_entityIdTagId" ON irCMS_modules_irModules_entityTag USING btree (entityId, tagId);
+CREATE INDEX "FKI_irCMS_modules_irModule_entityTag_userId"        ON irCMS_modules_irModules_entityTag USING btree (userId);
