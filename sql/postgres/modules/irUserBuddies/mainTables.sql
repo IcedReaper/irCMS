d63d05401c7d0d3c10c21c4cf1bd9dac36a4086d
IcedReaper <Kevin.Lang@gmx.de>
2013-10-31 13:47:10 +0100

    Added IAdded indicies to the tables of the module irUserBuddy

--- sql/postgres/modules/irUserBuddies/mainTables.sql
+++ sql/postgres/modules/irUserBuddies/mainTables.sql
@@ -1,15 +1,18 @@
 CREATE TABLE irCMS_modules_irUserBuddy_relation (
-    relationId serial,
-    userId     integer                  NOT NULL,
-    buddyId    integer                  NOT NULL,
+    relationId    serial,
+    userId        integer                  NOT NULL,
+    buddyId       integer                  NOT NULL,
     
-    addDate    timestamp with time zone          DEFAULT now(),
+    addDate       timestamp with time zone          DEFAULT now(),
+    confirmedDate timestamp with time zone,
     
     CONSTRAINT "PK_irCMS_modules_irUserBuddy_relation_relationId"  PRIMARY KEY (relationId),
     CONSTRAINT "UK_irCMS_modules_irUserBuddy_relation_connection"  UNIQUE      (userId, buddyId),
     CONSTRAINT "FK_irCMS_modules_irUserBuddy_relation_userId"      FOREIGN KEY (userId)          REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT "FK_irCMS_modules_irUserBuddy_relation_buddyUserId" FOREIGN KEY (buddyId)         REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+CREATE INDEX "FKI_irCMS_modules_irUserBuddy_relation_userId"  ON irCMS_modules_irUserBuddy_relation USING btree (userId);
+CREATE INDEX "FKI_irCMS_modules_irUserBuddy_relation_buddyId" ON irCMS_modules_irUserBuddy_relation USING btree (buddyId);
 
 CREATE TABLE irCMS_modules_irUserBuddy_message (
     messageId  serial,
@@ -23,10 +26,11 @@ CREATE TABLE irCMS_modules_irUserBuddy_message (
     subject    text                     NOT NULL,
     text       text                     NOT NULL,
     
-    
     CONSTRAINT "PK_irCMS_modules_irUserBuddy_message_messageId"  PRIMARY KEY (messageId),
     CONSTRAINT "FK_irCMS_modules_irUserBuddy_message_fromUserId" FOREIGN KEY (fromUserId) REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT "FK_irCMS_modules_irUserBuddy_message_toUserId"   FOREIGN KEY (toUserId)   REFERENCES irCMS_user (userId) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 );
-CREATE INDEX "FKI_irCMS_modules_irUserBuddy_message_toUnread"   ON irCMS_modules_userBuddy_message USING btree (toUserId, read);
-CREATE INDEX "FKI_irCMS_modules_irUserBuddy_message_fromUnread" ON irCMS_modules_userBuddy_message USING btree (fromUserId, read);
+CREATE INDEX "FKI_irCMS_modules_irUserBuddy_message_fromUserId" ON irCMS_modules_userBuddy_message USING btree (fromUserId);
+CREATE INDEX "FKI_irCMS_modules_irUserBuddy_message_toUserId"   ON irCMS_modules_userBuddy_message USING btree (toUserId);
+CREATE INDEX "IDX_irCMS_modules_irUserBuddy_message_toUnread"   ON irCMS_modules_userBuddy_message USING btree (toUserId, read);
+CREATE INDEX "IDX_irCMS_modules_irUserBuddy_message_fromUnread" ON irCMS_modules_userBuddy_message USING btree (fromUserId, read);
