fd0c5fddf893524fe48d1814da69a2af0293bde5
IcedReaper <Kevin.Lang@gmx.de>
2013-12-13 11:45:26 +0100

    Added core translation keys for some languages

--- sql/postgres/core/05a - translation_keys.sql
+++ sql/postgres/core/05a - translation_keys.sql
@@ -1,15 +1,24 @@
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.footer.link.anchor', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.footer.link.text', true, 1);
+
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.pageName', true, 1);
+
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.login.headline', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.login.login', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.login.register', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.login.userPanel', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.login.toAdminPanel', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.navigation.login.logout', true, 1);
+
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.permissionNotSufficient.headline', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.permissionNotSufficient.text', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.permissionNotSufficient.backToStart', true, 1);
+
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.wrongLogin.headline', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.wrongLogin.text', true, 1);
 INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.wrongLogin.backToStart', true, 1);
+
+INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.languages.german', true, 1);
+INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.languages.english', true, 1);
+INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.languages.french', true, 1);
+INSERT INTO irCMS_i18nKey (i18nName, active, userId) VALUES ('core.languages.spanish', true, 1);
